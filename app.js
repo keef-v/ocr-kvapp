0268eac6f152768e52a66cc266edc6247859746a
keerthi V <keef-vp@hotmail.com>
2020-06-09 22:02:59 +0100

    Created and tested endpoints for '/' and '/upload' .

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var multer = require('multer')
+var path = require('path')
 var upload = multer({ dest: 'uploads/' })
 
 var app = express()
@@ -7,12 +8,15 @@ var app = express()
 app.get('/status', (req, res) => {
     res.status(200).send('Server setup and running')
 })
+app.get('/', (req, res) => {
+    
+    res.status(200).sendFile(path.join(__dirname + '/form.html'));
+})
 
 app.post('/upload', upload.single('avatar'), function (req, res, next) {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
-
-    const file = req.file
+      const file = req.file
     if (!file) {
         const error = new Error('Please upload a file')
         error.httpStatusCode = 400;
--- app.spec.js
+++ app.spec.js
@@ -9,11 +9,17 @@ describe('Test the root path of express server', () => {
         expect(response.text).toBe('Server setup and running');
         done();
     });
-    test('It should response the GET method', async (done) => {
+    test('It should response error code 400 for no file present', async (done) => {
         const response = await request(app).post('/upload');
         expect(response.statusCode).toBe(400);
-
-
+        done();
+    });
+    test('It should return a form from address /', async (done) => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toMatch(/\/upload/);
+        expect(response.text).toMatch(/multipart\/form-data/);
+        
         done();
     });
 })
