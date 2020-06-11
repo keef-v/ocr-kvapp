0e7cc69512e4f39d65690d3966d8fb3ed7aeae84
keerthi V <keef-vp@hotmail.com>
2020-06-11 23:22:21 +0100

    got tesseract working

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var multer = require('multer')
 var path = require('path')
 var upload = multer({ dest: 'uploads/' })
+const { createWorker } = require('tesseract.js')
+const worker = createWorker({
+    logger: m => console.log(m),
+});
 
 var app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 app.get('/status', (req, res) => {
     res.status(200).send('Server setup and running')
@@ -13,16 +20,28 @@ app.get('/', (req, res) => {
     res.status(200).sendFile(path.join(__dirname + '/form.html'));
 })
 
-app.post('/upload', upload.single('avatar'), function (req, res, next) {
+app.post('/upload', upload.single('image2text'), function (req, res, next) {
     // req.file is the `avatar` file
     // req.body will hold the text fields, if there were any
       const file = req.file
     if (!file) {
         const error = new Error('Please upload a file')
         error.httpStatusCode = 400;
-        res.status(400).send('No Files were uploaded')
+        return res.status(400).send('No Files were uploaded')
+    }
+    async function getTextFromImage() {
+        await worker.load()
+        await worker.loadLanguage('eng')
+        await worker.initialize('eng')
+           
+        const { data: { text } } = await worker.recognize(file.originalname); // name of file is file.originalname
+        await worker.terminate()
+        
+        res.send(text)
     }
 
+    getTextFromImage();
+   
 })
 
 module.exports = app;
